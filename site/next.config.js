ccdc14d1d297cbe3948bfb8f72ec1be7389b6c49
野生程序猿 <126117131+GODU-LZR@users.noreply.github.com>
2025-02-11 17:19:42 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -1,11 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   env: {
     name: 'Kasm Technologies',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'http://localhost:5000/',  // 本地仓库的 URL，指向您的本地 Docker 仓库
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
