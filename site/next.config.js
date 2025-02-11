4d728764bedafebfd9fb290cee53fe585fb3d9a0
野生程序猿 <126117131+GODU-LZR@users.noreply.github.com>
2025-02-11 22:01:37 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'My Kasm Technologies',
+    description: 'My official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://godu-lzr.github.io/kasm-registry/',
+    contactUrl: 'https://godu-lzr.github.io/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
